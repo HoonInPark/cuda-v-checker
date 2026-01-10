6c3bf64c027513c36bee01f7b4bb6c1ad9c33b51
HoonInPark <hooninpark@gmail.com>
2026-01-11 00:22:03 +0900

    first we trying to build cuda lib check code

--- src/main.cpp
+++ src/main.cpp
@@ -4,33 +4,5 @@
 
 #include <iostream>
 
-// CUDA
-#include <cuda_runtime.h>
-// cuDNN
-#include <cudnn.h>
-// TensorRT
-#include "NvInfer.h"
-
 int main() {
-  std::cout << "=== CUDA/cuDNN/TensorRT Test ===\n";
-
-  // --- CUDA version ---
-  int deviceCount = 0;
-  cudaError_t cudaStatus = cudaGetDeviceCount(&deviceCount);
-  if (cudaStatus != cudaSuccess) {
-    std::cerr << "CUDA not found: " << cudaGetErrorString(cudaStatus) << "\n";
-  } else {
-    std::cout << "CUDA device count: " << deviceCount << "\n";
-  }
-
-  // --- cuDNN version ---
-  size_t cudnnVersion = cudnnGetVersion();
-  std::cout << "cuDNN version: " << cudnnVersion << "\n";
-
-  // --- TensorRT version ---
-  std::cout << "TensorRT version: " << NV_TENSORRT_MAJOR << "."
-            << NV_TENSORRT_MINOR << "." << NV_TENSORRT_PATCH << "\n";
-
-  std::cout << "Test completed successfully.\n";
-  return 0;
 }
