cc93e971bd89c555e6dfcd8a04d1f0e23811a14a
HoonInPark <hooninpark@gmail.com>
2026-01-11 17:29:06 +0900

    indent modified

--- src/main.cpp
+++ src/main.cpp
@@ -2,27 +2,29 @@
 // Created by changjoon-lee on 1/5/26.
 //
 
-#include <iostream>
 #include <cuda_runtime.h>
+#include <iostream>
 
 int main() {
-    int deviceCount = 0;
-    cudaError_t error = cudaGetDeviceCount(&deviceCount);
+  int deviceCount = 0;
+  cudaError_t error = cudaGetDeviceCount(&deviceCount);
 
-    if (error != cudaSuccess) {
-        std::cerr << "CUDA lib load FAILED : " << cudaGetErrorString(error) << std::endl;
-        return 1;
-    }
+  if (error != cudaSuccess) {
+    std::cerr << "CUDA lib load FAILED : " << cudaGetErrorString(error)
+              << std::endl;
+    return 1;
+  }
 
-    std::cout << "CUDA lib load SUCCESS!" << std::endl;
-    std::cout << "Device count : " << deviceCount << std::endl;
+  std::cout << "CUDA lib load SUCCESS!" << std::endl;
+  std::cout << "Device count : " << deviceCount << std::endl;
 
-    if (deviceCount > 0) {
-        cudaDeviceProp prop;
-        cudaGetDeviceProperties(&prop, 0);
-        std::cout << "GPU name : " << prop.name << std::endl;
-        std::cout << "CUDA Compute Capability: " << prop.major << "." << prop.minor << std::endl;
-    }
+  if (deviceCount > 0) {
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, 0);
+    std::cout << "GPU name : " << prop.name << std::endl;
+    std::cout << "CUDA Compute Capability: " << prop.major << "." << prop.minor
+              << std::endl;
+  }
 
-    return 0;
+  return 0;
 }
